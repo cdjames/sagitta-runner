6245cc250a0f03784b12fb7f8d18c7bbb3b50ebd
Collin James <mail@collinjam.es>
2017-07-19 17:53:53 -0400

    working with global vars now

--- ObjectBlueprints.cpp
+++ ObjectBlueprints.cpp
@@ -1,19 +1,18 @@
 #include "ObjectBlueprints.hpp"
 #include <iostream>
 
-ObjectBlueprints::ObjectBlueprints() {
-	// vector< vector< vector<ParticleCore> > > OBJ_THEMES.resize(SPORTS+1);
-	// vector< 			// x object types
-	// 	vector<			// y theme types
-	// 		vector<		// z blueprints
-	// 			vector<ParticleCore> // a single blueprint
-	// 			> > > OBJ_BLPRNTS;
-	createAllBlueprints();
-}
+vector<ParticleCore> DEF_SHIP_BP;
+vector<ParticleCore> DEF_BULLET_BP;
+vector<ParticleCore> DEF_EXP_BP;
+vector<ParticleCore> DEF_OBST_BP;
+// vector<ParticleCore> OBJECT;
+vector< 			// x object types
+	vector<			// y theme types
+		vector<		// z blueprints
+			vector<ParticleCore> // a single blueprint
+			> > > OBJ_BLPRNTS;
 
-ObjectBlueprints::~ObjectBlueprints() {}
-
-void ObjectBlueprints::createShipBlueprints() {
+void createShipBlueprints() {
 	vector< vector< vector<ParticleCore> > > OBJ_THEMES;
 	vector< vector<ParticleCore> > OBJECT_VARS;
 	vector< vector<ParticleCore> > OBJECT_VARS2;
@@ -58,7 +57,7 @@ void ObjectBlueprints::createShipBlueprints() {
 
 }
 
-void ObjectBlueprints::createBulletBlueprints() {
+void createBulletBlueprints() {
 	vector< vector< vector<ParticleCore> > > OBJ_THEMES;
 	vector< vector<ParticleCore> > OBJECT_VARS;
 	vector< vector<ParticleCore> > OBJECT_VARS2;
@@ -87,7 +86,7 @@ void ObjectBlueprints::createBulletBlueprints() {
 
 }
 
-void ObjectBlueprints::createExplosionBlueprints() {
+void createExplosionBlueprints() {
 	vector< vector< vector<ParticleCore> > > OBJ_THEMES;
 	vector< vector<ParticleCore> > OBJECT_VARS;
 	vector< vector<ParticleCore> > OBJECT_VARS2;
@@ -123,7 +122,7 @@ void ObjectBlueprints::createExplosionBlueprints() {
 
 }
 
-void ObjectBlueprints::createObstacleBlueprints() {
+void createObstacleBlueprints() {
 	vector< vector< vector<ParticleCore> > > OBJ_THEMES;
 	vector< vector<ParticleCore> > OBJECT_VARS;
 	vector< vector<ParticleCore> > OBJECT_VARS2;
@@ -156,35 +155,9 @@ void ObjectBlueprints::createObstacleBlueprints() {
 
 }
 
-void ObjectBlueprints::createAllBlueprints() {
-	// OBJ_BLPRNTS = vector< vector< vector< vector<ParticleCore> > > > (NUM_TYPES, 
-	// 					vector< vector< vector<ParticleCore> > > (NUM_THEMES,
-	// 					vector< vector<ParticleCore> > ));
-	// for (int i = 0; i < NUM_TYPES; i++)
-	// {
-	// 	for (int i = 0; i < NUM_THEMES; i++)
-	// 	{
-	// 		/* code */
-	// 	}
-	// }
+void createAllBlueprints() {
 	createShipBlueprints();
 	createBulletBlueprints();
 	createExplosionBlueprints();
 	createObstacleBlueprints();
 }
-
-// vector< vector<int> > DEF_OBST_BP (5, vector<int> (4, ));
-// vector< vector<int> > DEF_OBST_BP = {
-// 	{3, 3, -1, -1}, // first line is the height and width
-// 	{1, 0, 'x', 1}, // then x, y, symbol, color
-// 	{0, 1, 'X', 2},
-// 	{2, 1, 'x', 3},
-// 	{1, 2, 'X', 4}
-// };
-
-// /* number of themes in vector should equal the number of theme types in SagittaTypes */
-// vector< vector< vector<int> > > OBJ_THEMES = {
-// 	def_ship_bp,
-// 	def_ship_bp,
-// 	def_ship_bp
-// };
--- ObjectBlueprints.hpp
+++ ObjectBlueprints.hpp
@@ -6,37 +6,24 @@
 
 using std::vector;
 
-class ObjectBlueprints
-{
-public:
-	ObjectBlueprints();
-	~ObjectBlueprints();
+/* creator functions for templates (call from main) */
+void createShipBlueprints();
+void createBulletBlueprints();
+void createExplosionBlueprints();
+void createObstacleBlueprints();
+void createAllBlueprints();
 
-	/* creator functions for templates (call from main) */
-	void createShipBlueprints();
-	void createBulletBlueprints();
-	void createExplosionBlueprints();
-	void createObstacleBlueprints();
-	void createAllBlueprints();
+/* global object templates */
+extern vector<ParticleCore> DEF_SHIP_BP;
+extern vector<ParticleCore> DEF_BULLET_BP;
+extern vector<ParticleCore> DEF_EXP_BP;
+extern vector<ParticleCore> DEF_OBST_BP;
+// extern vector<ParticleCore> OBJECT;
 
-	/* global object templates */
-	vector<ParticleCore> DEF_SHIP_BP;
-	vector<ParticleCore> DEF_BULLET_BP;
-	vector<ParticleCore> DEF_EXP_BP;
-	vector<ParticleCore> DEF_OBST_BP;
-	vector<ParticleCore> OBJECT;
-	// extern vector<ParticleCore> FOOD_SHIP_BP;
-	// extern vector<ParticleCore> SPORTS_SHIP_BP;
+extern vector< 			// x object types
+		vector<			// y types
+			vector<		// z blueprints
+				vector<ParticleCore> > > > OBJ_BLPRNTS; // actual blueprints are vectors
 
-	// vector< vector<ParticleCore> > OBJECT_VARS;
-	// vector< vector< vector<ParticleCore> > > OBJ_THEMES;
 
-	vector< 			// x object types
-			vector<			// y types
-				vector<		// z blueprints
-					vector<ParticleCore> > > > OBJ_BLPRNTS; // actual blueprints are vectors
-
-	// extern vector< vector< vector<int> > > SHIP_THEMES;
-
-};
 #endif
